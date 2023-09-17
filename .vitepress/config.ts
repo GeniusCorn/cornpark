0a3bc0a867159b657a48bc2cba7ea141b8fb9229
GeniusCorn <hi@nicecorn.com>
2023-09-17 23:26:32 +0800

    添加一篇文章

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -290,8 +290,12 @@ function sidebarXiuxing() {
           link: '/修行/Nest 通关/9. 自定义装饰器',
         },
         {
-          text: '13. 如何切换到 Fastify ',
+          text: '13. 如何切换到 Fastify',
           link: '/修行/Nest 通关/13. 如何切换到 Fastify',
+        },
+        {
+          text: '14. Middleware',
+          link: '/修行/Nest 通关/14. Middleware',
         },
       ],
     },
